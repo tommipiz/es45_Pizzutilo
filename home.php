627fd1ea57ec0c65f15149e9d7dbf9d1fa941a90
tommaso pizzutilo <tommaso.pizzutilo@gmail.com>
2025-03-13 08:26:42 +0100

    update

--- home.php
+++ home.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form action="search.php" method="get">
-        <div class="divForm">
-            <h1>SCELTA ATTORI</h1>
+        <div>
+            <h1>Scegli gli attori</h1>
             <p>Inserisci un numero:</p>
             <input type="number" name="num" value="1">
             <br>
--- search.php
+++ search.php
@@ -7,29 +7,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SEARCH ATTORI</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Scelta attori</title>
 </head>
 <body>
     <?php
         $num = $_GET["num"];
 
         if ($num >=1) {
+
+            //Selezione degli attori dal database
             $sql = "SELECT a.CodAttore, a.Nome from attori as a ORDER BY a.Nome ASC LIMIT $num";
             $res = $conn->query($sql);
             if ($res->num_rows > 0) { 
-                echo "<div class='divShowData'>";
-                    echo "<h1 class='correct'>$res->num_rows ATTORI TROVATI</h1>";
+                echo "<div>";
+                    echo "<h1>$res->num_rows attori trovati</h1>";
                     $i=1;
                         while ($row = $res->fetch_assoc()) {
                             $codAttore = $row["CodAttore"];
                             $nome = $row["Nome"];
+
+                            //Per ogni attore, si conta il numero di film in cui ha recitato
                             $sql2 = "SELECT COUNT(r.CodFilm) as numFilmFatti FROM recita as r LEFT JOIN attori as a ON r.CodAttore = a.CodAttore WHERE a.CodAttore = $codAttore";
                             $res2 = $conn->query($sql2);
                                 if ($res2->num_rows > 0) {  
                                     $row_nf = $res2->fetch_assoc();
                                     $numFilm = $row_nf['numFilmFatti'];
                                     if ($numFilm >= 1) {
+
+                                        //Se l'attore ha recitato in almeno un film, vengono recuperati i dettagli
                                         $sql3 = "SELECT f.CodFilm, f.Titolo, f.AnnoProduzione FROM attori as a LEFT JOIN recita as r ON a.CodAttore = r.CodAttore LEFT JOIN film as f ON r.CodFilm = f.CodFilm WHERE a.CodAttore = $codAttore";
                                         $res3 = $conn->query($sql3);
                                         if ($res3->num_rows > 0) {
@@ -43,7 +48,7 @@
                         }
                     echo "<br>";
                     echo "<br>";
-                    echo "<a class='sendButton' href='home.html'>HOME</a>";
+                    echo "<a href='home.html'>HOME</a>";
                 echo "</div>";
             }
         } else {
@@ -67,16 +72,16 @@
             echo "<h1>ATTORE $i</h1>";
             echo "<p><b>Codice attore: </b>$codAt</p>";
             echo "<p><b>Nome attore: </b>$name</p>";
-            echo "<p class='error'>L'attore non ha fatto film</p>";
+            echo "<p>L'attore non ha fatto film</p>";
             echo "-----------------------------------------------------";
         }
 
         function showDataErr(){         
-            echo "<div class='divShowData'>";
+            echo "<div>";
                 echo "<h1 class='error'>NESSUN ATTORE TROVATO!</h1>";
                 echo "<br>";
                 echo "<br>";
-                echo "<a class='sendButton' href='home.html'>HOME</a>";
+                echo "<a href='home.html'>HOME</a>";
             echo "</div>";
         }   
        
